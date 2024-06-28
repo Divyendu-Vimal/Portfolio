60303b30338f2689f17e2de086594bafe73a885f
Divyendu Vimal <divyendu05052003@gmail.com>
2024-06-28 13:51:58 +0530

    aboutmeadded

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
+import About from './Components/About/About'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar/>
       <Home/>
+      <About/>
     </>
   )
 }
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import myImage from '/Images/me2.png'
+import { GiGraduateCap } from 'react-icons/gi'
+import { BsTelephoneForward } from 'react-icons/bs'
+import { TfiWorld } from 'react-icons/tfi'
+import './About.css'
+const About = () => {
+  return (
+    <section>
+        <h5>Get To Know</h5>
+        <h2>About Me</h2>
+
+        <div className='container about_container'>
+            <div className="about_me">
+                <div className="about_me_image">
+                    <img src={myImage} alt="Divyendu" />
+                </div>
+            </div>
+
+            <div className="about_content">
+                <div className="about_cards">
+                    <article className="about_card">
+                        <GiGraduateCap className='about_icon' />
+                        <h5>Degree</h5>
+                        <small>
+                            B.Tech Computer Science and Engineering <br />
+                            <i>Vellore Institute of Technology</i>
+                        </small>
+                    </article>
+
+                    <article className="about_card">
+                        <TfiWorld className='about_icon' />
+                        <h5>Location</h5>
+                        <small>
+                            Address...
+                        </small>
+                    </article>
+
+                    <article className="about_card">
+                        <BsTelephoneForward className='about_icon' />
+                        <h5>Contact</h5>
+                        <small>
+                            <ul>
+                                <li>Phone:</li>
+                                <li>Email:</li>
+                            </ul>
+                        </small>
+                    </article>
+                </div>
+
+                <p>
+                    Hi, I'm Divyendu Vimal, a skilled full-stack MERN developer with passion for building robust, scalable, and efficient applications. With expertise in MongoDB, Express, React, and Node.js. I craft seamless user experiences and robust backend architectures.
+                </p>
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default About
