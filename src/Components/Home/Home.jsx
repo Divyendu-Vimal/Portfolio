be5e58c5c637460394e71ff428060853565272a9
Divyendu Vimal <divyendu05052003@gmail.com>
2024-06-28 11:52:14 +0530

    homecomp

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
+import { SiLeetcode } from 'react-icons/si'
+import { SiCodeforces } from 'react-icons/si'
+import ME from '/Images/me.png';
+import './Home.css'
+
+const Home = () => {
+  return (
+    <header id='home'>
+        <div className='container home-container'>
+            <h5>Hello I'm</h5>
+            <h1>Divyendu Vimal</h1>
+            <h5 className='text-light'>Full-Stack Developer</h5>
+            <div className='cta'>
+                <a href="" download className='btn'>Download CV</a>
+                <a href="#contact" className='btn btn-primary'>Contact Me</a>
+            </div>
+
+            <div className="home_social">
+                <a href=""><BsLinkedin/></a>
+                <a href=""><BsGithub/></a>
+                <a href=""><SiLeetcode/></a>
+            </div>
+
+            <div className="me">
+                <img src={ME} alt="Divyendu" />
+            </div>
+
+            <a href="/contact" className='scroll_down'>Scroll Down</a>
+        </div>
+    </header>
+  )
+}
+
+export default Home;
